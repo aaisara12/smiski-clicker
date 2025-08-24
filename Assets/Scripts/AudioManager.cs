f6c5f8d1c04bdc009af387cb0d92894621ab5035
Ming <51458829+Do3naut@users.noreply.github.com>
2025-08-24 16:16:08 -0700

    audio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance = null;
+    AudioSource audioPlayer;
+
+    [SerializeField] AudioClip keyboard1;
+    [SerializeField] AudioClip keyboard2;
+    [SerializeField] AudioClip keyboard3;
+    [SerializeField] AudioClip buy;
+    [SerializeField] AudioClip sell;
+    [SerializeField] AudioClip newBanner;
+    [SerializeField] AudioClip rollTick;
+    [SerializeField] AudioClip rollFinal;
+
+    private void Awake()
+    {
+        Instance = this;
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayClick()
+    {
+        int option = Random.Range(0, 2);
+        switch (option)
+        {
+            case 0:
+                audioPlayer.PlayOneShot(keyboard1);
+                break;
+            case 1:
+                audioPlayer.PlayOneShot(keyboard2);
+                break;
+            case 2:
+                audioPlayer.PlayOneShot(keyboard3);
+                break;
+        }
+    }
+
+    public void PlayBuy()
+    {
+        audioPlayer.PlayOneShot(buy);
+    }
+
+    public void PlaySell()
+    {
+        audioPlayer.PlayOneShot(sell);
+    }
+
+    public void PlayNewBanner()
+    {
+        audioPlayer.PlayOneShot(newBanner);
+    }
+
+    public void PlayRollTick()
+    {
+        audioPlayer.PlayOneShot(rollTick);
+    }
+
+    public void PlayRollFinal()
+    {
+        audioPlayer.PlayOneShot(rollFinal);
+    }
+}
